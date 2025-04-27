2354f4c2464c9632cf4b5aee5d2117a9ade84ef8
Chan23TI <chandra23ti@mahasiswa.pcr.ac.id>
2025-04-27 18:51:06 +0700

    Instalasi Awal

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="flex justify-center p-6 bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+            <h1>Halo Selamat Datang di QRasa</h1>
+            <p>Noyokeste</p>
+        </div>
+    );
+}
