f1b7ce97361ddcc4ff892aa8cc327785a65158e6
Chan23TI <chandra23ti@mahasiswa.pcr.ac.id>
2025-05-01 13:30:47 +0700

    pembaruan

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,18 +1,26 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @viteReactRefresh
-    @vite('resources/js/app.jsx')
-    @inertiaHead
-</head>
+        <title inertia>{{ config('app.name', 'QRasa') }}</title>
 
-<body>
-    @inertia
-</body>
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="/images/favicon.png">
+        <link rel="apple-touch-icon" href="/images/apple-touch-icon.png">
 
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @routes
+        @viteReactRefresh
+        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+    </body>
 </html>
